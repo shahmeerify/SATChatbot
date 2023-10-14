5a189e8d96ad06840f51dc23e842ff8900129905
Muhammad Shahmeer <24100237@lums.edu.pk>
2023-10-14 12:25:31 +0500

    Updated Translation Script & Manual Fixing of translated text 1-250

--- Persian-SAT-Translation/Translation_Script.py
+++ Persian-SAT-Translation/Translation_Script.py
@@ -2,6 +2,8 @@ import re
 import time
 from googletrans import Translator
 import string
+import requests
+from urllib.parse import quote
 
 # Read the text file
 with open('Persian-SAT-Texts.txt', 'r', encoding='utf-8') as file:
@@ -16,17 +18,31 @@ double_quoted_pattern = r'"(.*?)"'
 double_quoted_contents = re.findall(double_quoted_pattern, input_text, re.MULTILINE | re.DOTALL)
 double_quoted_contents = [item for item in double_quoted_contents if not all(char in string.ascii_letters + ' ' for char in item)]
 
+# print(double_quoted_contents)
 # Initialize the Translator
 translator = Translator()
 
 translate_ultra_pro_max = {}
+source_language = "fa"
+target_language = "ur"
 
 # Iterate through the extracted contents and translate them
 for index, content in enumerate(double_quoted_contents):
-    print(f"Algorithm Loading: {index+1}/{len(double_quoted_contents)+1}")
+    print(f"Translating Text: {index+1}/{len(double_quoted_contents)+1}")
     try:
         # Translate the content into urdu
-        translated = translator.translate(content, dest='ur').text
+        print(content)
+        # translated = translator.translate(content, dest='ur', src='fa').text
+        url_encoded_text = quote(content)
+
+        url = f"https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&q={url_encoded_text}&sl={source_language}&tl={target_language}"
+
+        response = requests.get(url)
+
+        print(response.json()[0][0][0])
+
+        translated = response.json()[0][0][0]
+
         translate_ultra_pro_max[content] = translated
     except Exception as e:
         print("Translation error:", str(e))
